ba9479fc8e8efcd831fa36da6c7cd51aa9aa2c70
Sho Emoto <sho.emoto@gmail.com>
2019-11-17 02:04:25 +0900

    add project

---
+++ swf-CoreData-practice/swf-CoreData-practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swf-CoreData-practice
+//
+//  Created by 江本匠 on 2019/11/17.
+//  Copyright © 2019 S.Emoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ swf-CoreData-practice/swf-CoreData-practiceTests/swf_CoreData_practiceTests.swift
@@ -0,0 +1,34 @@
+//
+//  swf_CoreData_practiceTests.swift
+//  swf-CoreData-practiceTests
+//
+//  Created by 江本匠 on 2019/11/17.
+//  Copyright © 2019 S.Emoto. All rights reserved.
+//
+
+import XCTest
+@testable import swf_CoreData_practice
+
+class swf_CoreData_practiceTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
